be80e0862c0102c9bb8d36f19a4277f86e8d14a0
blinkbink <Asassins@y7mail.com>
2017-03-28 21:02:23 +0700

    Penambahan field user tabel guest
    Update Database

--- webq.sql
+++ webq.sql
@@ -27,6 +27,7 @@ CREATE TABLE `guest` (
   `usia` int(11) NOT NULL,
   `jenis_kelamin` varchar(15) NOT NULL,
   `kota` varchar(45) NOT NULL,
+  `user` varchar(45) NOT NULL,
   PRIMARY KEY (`idguess`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -98,4 +99,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-03-28 19:49:01
+-- Dump completed on 2017-03-28 21:01:16
