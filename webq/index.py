713cbbdc56e0c95bc10208683a05b4d94bf52651
blinkbink <Asassins@y7mail.com>
2017-04-01 19:58:39 +0700

    save data user, login session, connection db, Query soal
    Save data user ketika login, login session, koneksi dengan database,
    query soal secara random

--- webq/index.py
+++ webq/index.py
@@ -1,11 +1,60 @@
-import flask
-from flask import render_template
+from flask import Flask, render_template, session, redirect, url_for, request, escape
+from flask_sqlalchemy import SQLAlchemy
+import mysql.connector
+from mysql.connector import errorcode
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:0110@localhost/webq'
+#db = SQLAlchemy(app)
 
-@app.route("/")
-def hello():
-    return render_template('index.html', title='home')
+@app.route('/', methods=['GET','POST'])
+def index():
+    if 'user' in session:
+        db = mysql.connector.connect(host='localhost', port='3306', database='webq', user='root', password='0110')
+        cursor = db.cursor()
+        cursor.execute("SELECT question FROM question ORDER BY RAND() LIMIT 1")
+        data = cursor.fetchall()
+
+        username_session = escape(session['user']).capitalize()
+        number = 0
+        return render_template('web.html', user=username_session, data = data, number = number + 1)
+    else:
+        return redirect(url_for("login"))
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        session['user'] = request.form['user']
+        user = request.form['user']
+        usia = request.form['usia']
+        jenis_kelamin = request.form['jenis_kelamin']
+        kota = request.form['kota']
+
+        db = mysql.connector.connect(host='localhost', port='3306', database='webq', user='root', password='0110')
+        cursor = db.cursor()
+        cursor.execute("INSERT INTO guest(usia, jenis_kelamin, kota, user) VALUES (%s, %s, %s, %s)", (usia, jenis_kelamin, kota, user))
+        db.commit()
+        return redirect(url_for("index"))
+    else:
+        return render_template('index.html', title='home')
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('user', None)
+    return redirect(url_for('index'))
+
+@app.route('/result', methods=['POST', 'GET'])
+def result():
+    return render_template('result.html', title='Quiz')
+
+@app.errorhandler(404)
+def not_found(error):
+    return 'Page not found!', 404
+
+# set the secret key.  keep this really secret:
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
---
+++ webq/webq.sql
@@ -0,0 +1,102 @@
+-- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+--
+-- Host: localhost    Database: webq
+-- ------------------------------------------------------
+-- Server version	5.7.10-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `guest`
+--
+
+DROP TABLE IF EXISTS `guest`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `guest` (
+  `idguess` int(11) NOT NULL AUTO_INCREMENT,
+  `usia` int(11) NOT NULL,
+  `jenis_kelamin` varchar(15) NOT NULL,
+  `kota` varchar(45) NOT NULL,
+  `user` varchar(45) NOT NULL,
+  PRIMARY KEY (`idguess`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `guest`
+--
+
+LOCK TABLES `guest` WRITE;
+/*!40000 ALTER TABLE `guest` DISABLE KEYS */;
+/*!40000 ALTER TABLE `guest` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `question`
+--
+
+DROP TABLE IF EXISTS `question`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `question` (
+  `idquestion` int(11) NOT NULL AUTO_INCREMENT,
+  `question` longtext NOT NULL,
+  `answer` varchar(30) NOT NULL,
+  PRIMARY KEY (`idquestion`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `question`
+--
+
+LOCK TABLES `question` WRITE;
+/*!40000 ALTER TABLE `question` DISABLE KEYS */;
+INSERT INTO `question` VALUES (1,'Jumlah Kematian Akibat Bunuh Diri ditahun 2015','16'),(2,'Berapakah angka pengguna penyalahgunaan narkotika di daerah Jakarta Selatan ?','465'),(3,'Berapa jumlah guru SD Perempuan di Jakarta Timur pada tahun 2015 ?','8898'),(4,'Jumlah siswa SMK putus sekolah terbesar di DKI jakarta','Jakarta Timur 430 Siswa'),(5,'Penyebab Kematian Ibu DKI Jakarta','1'),(6,'Jumlah Halte Busway DKI Jakarta Pada Tahun 2011','207');
+/*!40000 ALTER TABLE `question` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `quiz`
+--
+
+DROP TABLE IF EXISTS `quiz`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `quiz` (
+  `idquiz` int(11) NOT NULL AUTO_INCREMENT,
+  `idquestion` int(11) NOT NULL,
+  `answer` varchar(45) NOT NULL,
+  PRIMARY KEY (`idquiz`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `quiz`
+--
+
+LOCK TABLES `quiz` WRITE;
+/*!40000 ALTER TABLE `quiz` DISABLE KEYS */;
+/*!40000 ALTER TABLE `quiz` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-03-28 21:01:16
