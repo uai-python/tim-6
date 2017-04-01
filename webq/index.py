08c41c3e4787d8c787846fedfea4114cfb269d8e
blinkbink <Asassins@y7mail.com>
2017-04-01 20:47:17 +0700

    Update database mysqlalchemy
    insert data

--- webq/index.py
+++ webq/index.py
@@ -1,12 +1,31 @@
 from flask import Flask, render_template, session, redirect, url_for, request, escape
 from flask_sqlalchemy import SQLAlchemy
 import mysql.connector
-from mysql.connector import errorcode
+
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:0110@localhost/webq'
-#db = SQLAlchemy(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:0110@localhost/webq'
+app.debug=True
+db = SQLAlchemy(app)
+
+class Guest(db.Model):
+    __tablename__ = 'guest'
+
+    idguess = db.Column(db.Integer, primary_key=True)
+    usia = db.Column(db.Integer)
+    jenis_kelamin = db.Column(db.String(15))
+    kota = db.Column(db.String(45))
+    user = db.Column(db.String(45))
+
+    def __init__(self, usia, jenis_kelamin, kota, user):
+        self.usia = usia
+        self.jenis_kelamin = jenis_kelamin
+        self.kota = kota
+        self.user = user
+
+    def __repr__(self):
+        return '<Guest %r>' % self.user
 
 @app.route('/', methods=['GET','POST'])
 def index():
@@ -31,10 +50,10 @@ def login():
         jenis_kelamin = request.form['jenis_kelamin']
         kota = request.form['kota']
 
-        db = mysql.connector.connect(host='localhost', port='3306', database='webq', user='root', password='0110')
-        cursor = db.cursor()
-        cursor.execute("INSERT INTO guest(usia, jenis_kelamin, kota, user) VALUES (%s, %s, %s, %s)", (usia, jenis_kelamin, kota, user))
-        db.commit()
+        guest = Guest(usia, jenis_kelamin, kota, user)
+        db.session.add(guest)
+        db.session.commit()
+
         return redirect(url_for("index"))
     else:
         return render_template('index.html', title='home')
