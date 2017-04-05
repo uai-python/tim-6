5db67b634463f77a93ec8a0b780e6c07cab36890
muhammadbawazeer <m7mdbawazeer@gmail.com>
2017-04-05 21:45:20 +0700

    Add files via upload
    update

--- app/index.py
+++ app/index.py
@@ -13,11 +13,25 @@ db = SQLAlchemy(app)
 @app.route('/', methods=['GET','POST'])
 def index():
     if 'user' in session:
-        fetch_data = Question.query.order_by(func.rand()).limit(1).all()
         user = session['user']
-        return render_template('web.html', data=fetch_data, user=user)
+        return render_template('index.html', user=user)
     else:
         return redirect(url_for("login"))
+
+@app.route('/link', methods=['GET', 'POST'])
+def link():
+    fetch_data = Question.query.order_by(func.rand()).limit(1).all()
+    user = session['user']
+    Total_Soal = 0
+    Total_jawab_benar = 0
+    if request.method == 'POST':
+        Total_Soal = request.form['total']
+        Total_jawab_benar = 0
+
+        fetch_data = Question.query.order_by(func.rand()).limit(1).all()
+        user = session['user']
+        return render_template('web.html', data=fetch_data, user=user, Total_Soal=Total_Soal, Total_jawab_benar=Total_jawab_benar)
+    return render_template('web.html', data=fetch_data, user=user, Total_Soal=Total_Soal, Total_jawab_benar=Total_jawab_benar)
 
 @app.route('/login', methods=['GET','POST'])
 def login():
@@ -34,24 +48,38 @@ def login():
 
         return redirect(url_for("index"))
     else:
-        return render_template('index.html', title='home')
+        return render_template('login.html', title='home')
 
 @app.route('/cek', methods=['GET','POST'])
 def cek():
-    if request.method == 'POST':
-        jawaban = request.form['jawaban']
+    total = 0
+    hasil = 0
 
-    return render_template('web.html', title='Cek')
+
+    if request.method == 'POST':
+        jawab_asli = request.form['jawaban']
+        ask = request.form['pertanyaan']
+        jawab_user = request.form['answer']
+        user = session['user']
+
+        jumlah = int(request.form['soal_ke']) + 1
+
+        if jumlah > 10:
+            return render_template('result.html', title='Cek', jawab_asli=jawab_asli, jawab_user=jawab_user, ask=ask,
+                                   user=user, total=total, hasil=hasil, jumlah=jumlah)
+
+        if jawab_asli == jawab_user:
+            hasil = hasil + 1
+        elif jawab_asli != jawab_user:
+            hasil = hasil + 0
+
+    return render_template('cek.html', title='Cek', jawab_asli=jawab_asli, jawab_user=jawab_user, ask=ask, user=user, total=total, hasil=hasil, jumlah=jumlah)
 
 @app.route('/logout')
 def logout():
     # remove the username from the session if it's there
     session.pop('user', None)
     return redirect(url_for('index'))
-
-@app.route('/result', methods=['POST', 'GET'])
-def result():
-    return render_template('dashboard.html', title='Dashboard')
 
 @app.errorhandler(404)
 def not_found(error):
