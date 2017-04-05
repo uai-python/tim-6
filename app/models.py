586cee056ebe7fc4f02499726ade9fe7b9d6cf87
blinkbink <Asassins@y7mail.com>
2017-04-05 21:55:51 +0700

    Update

--- app/models.py
+++ app/models.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from config import db
 
 class Guest(db.Model):
@@ -53,39 +52,3 @@ class Quiz(db.Model):
 
     def __repr__(self):
         return '<Quiz %r>' % self.user
-=======
-from config import db
-
-class Guest(db.Model):
-    __tablename__ = 'guest'
-
-    idguess = db.Column(db.Integer, primary_key=True)
-    usia = db.Column(db.Integer)
-    jenis_kelamin = db.Column(db.String(15))
-    kota = db.Column(db.String(45))
-    user = db.Column(db.String(45))
-
-    def __init__(self, usia, jenis_kelamin, kota, user):
-        self.usia = usia
-        self.jenis_kelamin = jenis_kelamin
-        self.kota = kota
-        self.user = user
-
-    def __repr__(self):
-        return '<Guest %r>' % self.user
-
-
-class Question(db.Model):
-    __tablename__ = 'question'
-
-    idquestion = db.Column(db.Integer, primary_key=True)
-    question = db.Column(db.String)
-    answer = db.Column(db.String(30))
-
-    def __init__(self, question, answer):
-        self.question = question
-        self.answer = answer
-
-    def __repr__(self):
-        return '<Question %r>' % self.question
->>>>>>> origin/master
